c004705246f6c8378e501955ae24ecffc1e6b2b9
Bernardo Hille <hille.bernardo@gmail.com>
2025-10-01 19:26:06 +0000

    Vamos rever a responsividade em um smartphone virado ele esta verticaliz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import { Logo } from '@/components/logo';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col md:flex-row">
+    <main className="flex min-h-screen flex-row">
       {/* Left Panel */}
-      <div className="flex w-full flex-col items-center justify-center gap-4 bg-background p-8 text-center md:w-1/2">
-        <Logo className="h-72 w-72" />
+      <div className="flex w-1/2 flex-col items-center justify-center gap-4 bg-background p-8 text-center">
+        <Logo className="h-96 w-96" />
       </div>
 
       {/* Right Panel */}
-      <div className="flex w-full items-center justify-center bg-panel-right p-8 md:w-1/2">
+      <div className="flex w-1/2 items-center justify-center bg-panel-right p-8">
         <div className="flex flex-col items-center gap-6">
           <Button
             size="lg"
