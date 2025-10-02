e70c0eb606de1a5506c664f6212fdef59a378737
Bernardo Hille <hille.bernardo@gmail.com>
2025-10-02 20:59:25 +0000

    Olha só esta ficando bem errado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
       <head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
