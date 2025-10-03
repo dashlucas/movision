00f35655754ac272a73e8b9742fbbd5837fbddba
Bernardo Hille <hille.bernardo@gmail.com>
2025-10-03 14:25:40 +0000

    Faça essa rolagem maior

--- src/app/posicionamento/page.tsx
+++ src/app/posicionamento/page.tsx
@@ -78,7 +78,7 @@ export default function PosicionamentoPage() {
   }, [countdown, hasCameraPermission, router]);
 
   return (
-    <main className="h-[101svh] w-screen bg-black">
+    <main className="h-[110svh] w-screen bg-black">
       <div className="fixed inset-0 h-[100svh] w-screen">
         {hasCameraPermission === undefined && (
           <div className="absolute inset-0 z-20 flex items-center justify-center bg-black/70 text-white">
@@ -138,7 +138,7 @@ export default function PosicionamentoPage() {
           Browser
         </div>
       </div>
-      <div className="h-[1svh] w-full" />
+      <div className="h-[10svh] w-full" />
     </main>
   );
 }
