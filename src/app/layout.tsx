dc7ce6addfc2bb6392acb0ac599bc14b9fc6b439
Bernardo Hille <hille.bernardo@gmail.com>
2025-10-03 13:58:50 +0000

    Para consertar a faixa branca em pwa
    Permitir ocupar a área segura e co

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,12 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1, viewport-fit=cover"
         />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <meta name="theme-color" content="#49416D" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
