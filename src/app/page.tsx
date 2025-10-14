13991524699f2646e20b0e187d8ebede8000c41c
Bernardo Hille <hille.bernardo@gmail.com>
2025-10-14 17:06:59 +0000

    Coloque mais para biaxo uns 10% o circulo dentro do cronometro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -634,7 +634,7 @@ function JogoView({
                 width: 'calc(8rem - 18px)',
                 height: 'calc(8rem - 18px)',
                 left: 'calc(2rem + 9px)',
-                top: 'calc(2rem + 9px)'
+                top: 'calc(2rem + 9px + 10%)'
               }}
             />
           <div className="absolute left-8 top-8 h-32 w-32">
